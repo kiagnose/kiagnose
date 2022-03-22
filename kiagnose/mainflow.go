8715f4cb310847554a5f05e32e6750d26e0dd854
Or Mergi <ormergi@redhat.com>
2022-03-22 18:41:47 +0200

    kiagnose/mainflow: Introduce basic skeleton
    Introduce kiagnose mainflow skeleton for launching and managing
    checkups lifecycle (e.g: setup, teardown, result reporting).
    
    Signed-off-by: Or Mergi <ormergi@redhat.com>

--- kiagnose/mainflow.go
+++ kiagnose/mainflow.go
@@ -19,6 +19,45 @@
 
 package kiagnose
 
-func Run() error {
+import (
+	"fmt"
+)
+
+type launcher interface {
+	Setup() error
+	Run() error
+	Teardown() error
+	Report() error
+}
+
+func Run(launcher launcher) (runErr error) {
+	defer func() {
+		if reportErr := launcher.Report(); reportErr != nil {
+			if runErr != nil {
+				runErr = fmt.Errorf("%v, %v", runErr, reportErr)
+			} else {
+				runErr = reportErr
+			}
+		}
+	}()
+
+	if err := launcher.Setup(); err != nil {
+		return err
+	}
+
+	defer func() {
+		if teardownErr := launcher.Teardown(); teardownErr != nil {
+			if runErr != nil {
+				runErr = fmt.Errorf("%v, %v", runErr, teardownErr)
+			} else {
+				runErr = teardownErr
+			}
+		}
+	}()
+
+	if err := launcher.Run(); err != nil {
+		return err
+	}
+
 	return nil
 }
