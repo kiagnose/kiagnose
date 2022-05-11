f9b7bdf06c4dddc56598077cffa49818abec532b
Or Mergi <ormergi@redhat.com>
2022-05-11 14:32:08 +0300

    kubevirt-vm-latency: Introduce vmi package
    The 'vmi' package provides a factory for generating Kubevirt[1] VMs that will
    be used by the checkup.
    It is built from Kubevirt's 'libvmi' [2] and 'libnet' [3] packages, stripped down
    to meet the checkup's needs.
    
    Also go.mod file is updated, no new files were added:
    - 'kubevirt.io/api' and 'k8s.io/api' are no longer indirect import since they are
       being used directly.
    - 'sigs.k8s.io/yaml' required by vmi/cloudinit.go.
    
    [1] https://kubevirt.io
    [2] https://github.com/kubevirt/kubevirt/blob/v0.53.0/tests/libvmi
    [3] https://github.com/kubevirt/kubevirt/blob/v0.53.0/tests/libnet
    
    Signed-off-by: Or Mergi <ormergi@redhat.com>

---
+++ checkups/kubevirt-vm-latency/vmlatency/internal/vmi/spec.go
@@ -0,0 +1,209 @@
+/*
+ * This file is part of the kiagnose project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2022 Red Hat, Inc.
+ *
+ */
+
+// Package vmi source https://github.com/kubevirt/kubevirt/tree/v0.53.0/tests/libvmi
+package vmi
+
+import (
+	k8scorev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	k8smetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	kvcorev1 "kubevirt.io/api/core/v1"
+)
+
+// Option represents an action that enables an option.
+type Option func(vmi *kvcorev1.VirtualMachineInstance)
+
+// newBaseVmi instantiates a new VMI configuration,
+// building its properties based on the specified With* options.
+func newBaseVmi(name string, opts ...Option) *kvcorev1.VirtualMachineInstance {
+	vmi := &kvcorev1.VirtualMachineInstance{
+		TypeMeta: k8smetav1.TypeMeta{
+			Kind:       kvcorev1.VirtualMachineInstanceGroupVersionKind.Kind,
+			APIVersion: kvcorev1.GroupVersion.String(),
+		},
+		ObjectMeta: k8smetav1.ObjectMeta{
+			Name: name,
+		},
+		Spec: kvcorev1.VirtualMachineInstanceSpec{},
+	}
+
+	for _, f := range opts {
+		f(vmi)
+	}
+
+	return vmi
+}
+
+// withTerminationGracePeriodSecond sets TerminationGracePeriodSecond.
+func withTerminationGracePeriodSecond(duration int64) Option {
+	return func(vmi *kvcorev1.VirtualMachineInstance) {
+		vmi.Spec.TerminationGracePeriodSeconds = &duration
+	}
+}
+
+// WithNodeSelector ensures that the VMI gets scheduled on the specified node.
+func WithNodeSelector(nodeName string) Option {
+	return func(vmi *kvcorev1.VirtualMachineInstance) {
+		if vmi.Spec.NodeSelector == nil {
+			vmi.Spec.NodeSelector = map[string]string{}
+		}
+		vmi.Spec.NodeSelector[k8scorev1.LabelHostname] = nodeName
+	}
+}
+
+// withResourceMemory specifies the vmi memory resource.
+func withResourceMemory(value string) Option {
+	return func(vmi *kvcorev1.VirtualMachineInstance) {
+		vmi.Spec.Domain.Resources.Requests = k8scorev1.ResourceList{
+			k8scorev1.ResourceMemory: resource.MustParse(value),
+		}
+	}
+}
+
+// withRng adds 'rng' to the vmi devices.
+func withRng() Option {
+	return func(vmi *kvcorev1.VirtualMachineInstance) {
+		vmi.Spec.Domain.Devices.Rng = &kvcorev1.Rng{}
+	}
+}
+
+// WithMultusNetwork adds network with Multus network source.
+func WithMultusNetwork(networkName, netAttachDefName string) Option {
+	return func(vmi *kvcorev1.VirtualMachineInstance) {
+		vmi.Spec.Networks = append(vmi.Spec.Networks, multusNetwork(networkName, netAttachDefName))
+	}
+}
+
+func multusNetwork(name, netAttachDefName string) kvcorev1.Network {
+	return kvcorev1.Network{
+		Name: name,
+		NetworkSource: kvcorev1.NetworkSource{
+			Multus: &kvcorev1.MultusNetwork{
+				NetworkName: netAttachDefName,
+			},
+		},
+	}
+}
+
+// WithCloudInitNoCloudNetworkData adds cloud-init no-cloud network data.
+func WithCloudInitNoCloudNetworkData(networkData string) Option {
+	return func(vmi *kvcorev1.VirtualMachineInstance) {
+		const volumeName = "cloudinit"
+		vmi.Spec.Volumes = append(vmi.Spec.Volumes, newCloudinitVolumeWithNetworkData(volumeName, networkData))
+		vmi.Spec.Domain.Devices.Disks = append(vmi.Spec.Domain.Devices.Disks, newDisk(volumeName))
+	}
+}
+
+func newCloudinitVolumeWithNetworkData(name, networkData string) kvcorev1.Volume {
+	return kvcorev1.Volume{
+		Name: name,
+		VolumeSource: kvcorev1.VolumeSource{
+			CloudInitNoCloud: &kvcorev1.CloudInitNoCloudSource{
+				NetworkData: networkData,
+			},
+		},
+	}
+}
+
+func newDisk(name string) kvcorev1.Disk {
+	return kvcorev1.Disk{
+		Name: name,
+		DiskDevice: kvcorev1.DiskDevice{
+			Disk: &kvcorev1.DiskTarget{
+				Bus: kvcorev1.DiskBusVirtio,
+			},
+		},
+	}
+}
+
+func withContainerDiskImage(name string) Option {
+	return func(vmi *kvcorev1.VirtualMachineInstance) {
+		diskName := "disk0"
+		vmi.Spec.Domain.Devices.Disks = append(vmi.Spec.Domain.Devices.Disks, newDisk(diskName))
+		vmi.Spec.Volumes = append(vmi.Spec.Volumes, newContainerDiskVolume(diskName, name))
+	}
+}
+
+func newContainerDiskVolume(name, image string) kvcorev1.Volume {
+	return kvcorev1.Volume{
+		Name: name,
+		VolumeSource: kvcorev1.VolumeSource{
+			ContainerDisk: &kvcorev1.ContainerDiskSource{
+				Image: image,
+			},
+		},
+	}
+}
+
+// WithInterface adds an interface.
+func WithInterface(iface kvcorev1.Interface) Option {
+	return func(vmi *kvcorev1.VirtualMachineInstance) {
+		vmi.Spec.Domain.Devices.Interfaces = append(vmi.Spec.Domain.Devices.Interfaces, iface)
+	}
+}
+
+// interfaceOption represents an action that enables an option on a VMI interface.
+type interfaceOption func(vmi *kvcorev1.Interface)
+
+// NewInterface instantiates a new VMI interface configuration,
+// building its properties based on the specified With* options.
+func NewInterface(name string, opts ...interfaceOption) kvcorev1.Interface {
+	iface := &kvcorev1.Interface{Name: name}
+
+	for _, f := range opts {
+		f(iface)
+	}
+
+	return *iface
+}
+
+// WithMacAddress set the interface with custom MAC address.
+func WithMacAddress(macAddress string) interfaceOption {
+	return func(iface *kvcorev1.Interface) {
+		iface.MacAddress = macAddress
+	}
+}
+
+// WithSriovBinding set the interface with SR-IOV binding method.
+func WithSriovBinding() interfaceOption {
+	return func(iface *kvcorev1.Interface) {
+		iface.InterfaceBindingMethod = kvcorev1.InterfaceBindingMethod{
+			SRIOV: &kvcorev1.InterfaceSRIOV{},
+		}
+	}
+}
+
+func NewAlpine(name string, opts ...Option) *kvcorev1.VirtualMachineInstance {
+	const (
+		memory                                     = "128Mi"
+		alpineContainerDiskImage                   = "quay.io/kubevirt/alpine-with-test-tooling-container-disk:v0.53.0"
+		defaultTerminationGracePeriodSeconds int64 = 5
+	)
+	latencyCheckOpts := []Option{
+		withContainerDiskImage(alpineContainerDiskImage),
+		withTerminationGracePeriodSecond(defaultTerminationGracePeriodSeconds),
+		withResourceMemory(memory),
+		withRng(),
+	}
+	latencyCheckOpts = append(latencyCheckOpts, opts...)
+
+	return newBaseVmi(name, latencyCheckOpts...)
+}
