0680a2ff35351e876cf704db9068c55f20240034
Edward Haas <edwardh@redhat.com>
2022-10-25 21:13:55 +0300

    checkups, vm-latency: Vendor dependent modules
    This change focuses on vendoring the dependencies to assist in the
    review process of the following commit.
    
    It is a result of the checkup module using packages from the kiagnose
    module.
    
    Signed-off-by: Edward Haas <edwardh@redhat.com>

---
+++ checkups/kubevirt-vm-latency/vendor/github.com/kiagnose/kiagnose/kiagnose/configmap/configmap.go
@@ -0,0 +1,50 @@
+/*
+ * This file is part of the kiagnose project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2022 Red Hat, Inc.
+ *
+ */
+
+package configmap
+
+import (
+	"context"
+	"log"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+func Create(client kubernetes.Interface, cm *corev1.ConfigMap) (*corev1.ConfigMap, error) {
+	createdConfigMap, err := client.CoreV1().ConfigMaps(cm.Namespace).Create(context.Background(), cm, metav1.CreateOptions{})
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("ConfigMap '%s/%s' successfully created", cm.Namespace, cm.Name)
+	return createdConfigMap, nil
+}
+
+func Get(client kubernetes.Interface, namespace, name string) (*corev1.ConfigMap, error) {
+	return client.CoreV1().ConfigMaps(namespace).Get(context.Background(), name, metav1.GetOptions{})
+}
+
+func Update(client kubernetes.Interface, configMap *corev1.ConfigMap) (*corev1.ConfigMap, error) {
+	return client.CoreV1().ConfigMaps(configMap.Namespace).Update(context.Background(), configMap, metav1.UpdateOptions{})
+}
+
+func Delete(client kubernetes.Interface, namespace, name string) error {
+	return client.CoreV1().ConfigMaps(namespace).Delete(context.Background(), name, metav1.DeleteOptions{})
+}
