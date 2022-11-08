8779b4500a9718d03e76e1c64717a01e82f7db2c
Or Mergi <ormergi@redhat.com>
2022-11-08 15:31:49 +0200

    vmlatemcy: Update golangci-lint
    Signed-off-by: Or Mergi <ormergi@redhat.com>

--- checkups/kubevirt-vm-latency/automation/make.sh
+++ checkups/kubevirt-vm-latency/automation/make.sh
@@ -79,7 +79,7 @@ if [ "${ARGCOUNT}" -eq "0" ] ; then
 fi
 
 if [ -n "${OPT_LINT}" ]; then
-    golangci_lint_version=v1.45.2
+    golangci_lint_version=v1.50.1
     if [ ! -f $(go env GOPATH)/bin/golangci-lint ]; then
         curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin $golangci_lint_version
     fi
